9b9cb611d8bae2c6459cffa1c7d83cd5913cb3f8
noisykeyboard <webmaster@shitposters.club>
2021-04-03 18:02:29 -0400

    Move ports to variables

--- remote.sh
+++ remote.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-websocketd -maxforks=1 -port 8990 -- nc localhost 6859
+MINDUSTRY_PORT=6859
+LISTEN_PORT=8990
+
+websocketd -maxforks=1 -port $LISTEN_PORT -- nc localhost $MINDUSTRY_PORT
