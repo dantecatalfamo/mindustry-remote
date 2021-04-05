44c9a1148ab5531f540d72abef35353f18199f32
Dante Catalfamo <dante.catalfamo@shopify.com>
2021-04-05 14:22:25 -0400

    Clean up JS a little

--- html/remote.js
+++ html/remote.js
@@ -2,22 +2,8 @@ const WEBSOCKET = 'ws://localhost:8990';
 
 const output = document.getElementById("output");
 const input = document.getElementById("input");
-
-function log(msg) {
-  output.textContent +=
-    msg.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '')
-       .replace(/[^\x20-\x7E]/g, '')
-    + "\n";
-  output.scrollTop = output.scrollHeight;
-}
-
-function send() {
-  log("> " + input.value);
-  ws.send(input.value);
-  input.value = "";
-}
-
 const ws = new WebSocket(WEBSOCKET);
+
 ws.onopen = function() {
   log('** Connected to server **');
 };
@@ -37,4 +23,18 @@ input.addEventListener('keyup', (event) => {
   }
 });
 
-console.log("Hello!");
+function clean(msg) {
+  return msg.replace(/[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g, '')  // Remove ANSI escape codes
+            .replace(/[^\x20-\x7E]/g, '');  // Remove non-printable characters
+}
+
+function log(msg) {
+  output.textContent += clean(msg) + "\n";
+  output.scrollTop = output.scrollHeight;
+}
+
+function send() {
+  log("> " + input.value);
+  ws.send(input.value);
+  input.value = "";
+}
